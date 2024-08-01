2f3ac69ffc1659b75fbd80a6b2ee3d448c3cdaf1
tomohikoseven <tomohikoseven0905@gmail.com>
2024-08-01 16:20:58 +0900

    Sitemapの作成．

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,32 +3,33 @@ import starlight from '@astrojs/starlight';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '数学の主張',
-			social: {
-				github: 'https://github.com/tomohikoseven',
-				'x.com': 'https://x.com/tomohikoseven',
-			},
-			sidebar: [
-				{
-					label: '実数論',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Software',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
-	}
+  integrations: [starlight({
+    title: '数学の主張',
+    social: {
+      github: 'https://github.com/tomohikoseven',
+      'x.com': 'https://x.com/tomohikoseven'
+    },
+    sidebar: [{
+      label: '実数論',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        slug: 'guides/example'
+      }]
+    }, {
+      label: 'Software',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
 });
