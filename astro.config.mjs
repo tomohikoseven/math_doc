551ee63463e6e33c796cb61352b07bf3b2c4307c
tomohikoseven <tomohikoseven0905@gmail.com>
2024-08-06 06:56:07 +0900

    katex.cssをサイト全体に追加．

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,15 @@ export default defineConfig({
           content: 'EC7D-cNrvKzXzsz8OePb5W1ZKSZ_-Bb5pTv9iMELkRE'
         },
       },
+      {
+        tag:'link',
+        attrs:{
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css',
+          integrity: 'sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+',
+          crossorigin: 'anonymous'
+        }
+      },
     ],
     title: '数学の主張',
     social: {
--- src/content/docs/real_number/example.md
+++ src/content/docs/real_number/example.md
@@ -2,7 +2,6 @@
 title: Example Guide
 description: A guide in my new Starlight docs site.
 ---
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+" crossorigin="anonymous">
 Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
 Writing a good guide requires thinking about what your users are trying to do.
 
