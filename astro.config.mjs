d985dee5aff39ef17dc0f0a70e2283a01774c97d
tomohikoseven <tomohikoseven0905@gmail.com>
2024-12-05 17:06:55 +0900

    siteのURL末尾にスラッシュ追加．

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://math-doc.pages.dev',
+  site: 'https://math-doc.pages.dev/',
   integrations: [starlight({
     favicon: '/favicon.ico',
     head:[
