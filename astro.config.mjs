ceea21b560843c2756e5420a4a2f5db211f98617
tomohikoseven <tomohikoseven0905@gmail.com>
2026-02-10 15:01:50 +0900

    インタースティシャル型広告の表示対応．

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,18 @@ import d2 from 'astro-d2';
 
 import starlightLinksValidator from 'starlight-links-validator';
 import { GOOGLE_ANALYTICS } from './src/libs/head';
-import { REAL_NUMBER,
-          LINEAR_ALGEBRA,
-          MATHEMATICS,
-          MATH_DIALY,
-          SOFTWARE,
-          ABOUT_MY_WEBSITE,
-          HIGH_SCHOOL_MATH,
-          LINEAR_ALGEBRA_KAWAKUBO,
-          MCGRAW_HILL_CALCULUS_UPPER,
-          OTHER_THAN_MATH } from './src/libs/sidebar';
+import {
+  REAL_NUMBER,
+  LINEAR_ALGEBRA,
+  MATHEMATICS,
+  MATH_DIALY,
+  SOFTWARE,
+  ABOUT_MY_WEBSITE,
+  HIGH_SCHOOL_MATH,
+  LINEAR_ALGEBRA_KAWAKUBO,
+  MCGRAW_HILL_CALCULUS_UPPER,
+  OTHER_THAN_MATH
+} from './src/libs/sidebar';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -31,8 +33,8 @@ export default defineConfig({
         lang: 'ja', // langはルートロケールに必要です
       },
     },
-    head:[
-     ...GOOGLE_ANALYTICS,
+    head: [
+      ...GOOGLE_ANALYTICS,
     ],
     plugins: [starlightLinksValidator()],
     title: '数学の主張',
@@ -44,37 +46,37 @@ export default defineConfig({
       Sidebar: './src/components/starlight/Sidebar.astro',
       MarkdownContent: './src/components/starlight/MarkdownContent.astro',
     },
-    tableOfContents: true, 
-    lastUpdated: true, 
-    disable404Route: true, 
+    tableOfContents: true,
+    lastUpdated: true,
+    disable404Route: true,
     //i18n: {
     //  routing: {
     //    prefixDefaultLocale: false,
     //  }
     //},
-    customCss: [ './src/styles/custom.css' ] ,
+    customCss: ['./src/styles/custom.css'],
     social: [
-      { icon:'github', label:'GitHub', href: 'https://github.com/tomohikoseven' },
-      { icon: 'x.com', label:'X', href: 'https://x.com/tomohikoseven' },
-      { icon:'instagram', label:'Instagram', href: 'https://www.instagram.com/nagasetomohiko/'},
+      { icon: 'github', label: 'GitHub', href: 'https://github.com/tomohikoseven' },
+      { icon: 'x.com', label: 'X', href: 'https://x.com/tomohikoseven' },
+      { icon: 'instagram', label: 'Instagram', href: 'https://www.instagram.com/nagasetomohiko/' },
     ],
     sidebar: [
-      REAL_NUMBER, 
-      MCGRAW_HILL_CALCULUS_UPPER, 
+      REAL_NUMBER,
+      MCGRAW_HILL_CALCULUS_UPPER,
       LINEAR_ALGEBRA,
-      LINEAR_ALGEBRA_KAWAKUBO, 
-      MATHEMATICS, 
+      LINEAR_ALGEBRA_KAWAKUBO,
+      MATHEMATICS,
       MATH_DIALY,
       HIGH_SCHOOL_MATH,
-      SOFTWARE, 
-      OTHER_THAN_MATH, 
+      SOFTWARE,
+      OTHER_THAN_MATH,
       ABOUT_MY_WEBSITE
     ]
-  }), 
-  d2(), 
+  }),
+  d2(),
   sitemap()],
   markdown: {
     remarkPlugins: [remarkMath, emoji],
-    rehypePlugins: [ rehypeTypst ],
+    rehypePlugins: [rehypeTypst],
   }
 });
--- src/libs/head.ts
+++ src/libs/head.ts
@@ -1,52 +1,52 @@
 // Adsence 審査用コード
 export const GOOGLE_SITE_VERIFICATION = {
-        tag: 'meta',
-        attrs: {
-          name: 'google-site-verification',
-          content: 'EC7D-cNrvKzXzsz8OePb5W1ZKSZ_-Bb5pTv9iMELkRE'
-        }
-    };
+  tag: 'meta',
+  attrs: {
+    name: 'google-site-verification',
+    content: 'EC7D-cNrvKzXzsz8OePb5W1ZKSZ_-Bb5pTv9iMELkRE'
+  }
+};
 
 // Google Adsence
 export const GOOGLE_ADSENSE = {
-        tag: 'meta',
-        attrs: {
-          name: 'google-adsense-account',
-          content: 'ca-pub-7031713239242378'
-        }
-      };
+  tag: 'meta',
+  attrs: {
+    name: 'google-adsense-account',
+    content: 'ca-pub-7031713239242378'
+  }
+};
 
 // Google Analytics
 export const GOOGLE_ANALYTICS = [{
-        tag: `script`,
-        attrs: {
-          async: true,
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-GLTH0LBWDY',
-        }
-      },
-      {
-        tag: 'script',
-        content: `
+  tag: `script`,
+  attrs: {
+    async: true,
+    src: 'https://www.googletagmanager.com/gtag/js?id=G-GLTH0LBWDY',
+  }
+},
+{
+  tag: 'script',
+  content: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-GLTH0LBWDY');
  `
-      }];
+}];
 
 export const BUY_ME_A_COFFEE = {
-    tag: 'script',
-    attrs: {
-        'data-name': 'BMC-Widget',
-        'data-cfasync': 'false',
-        src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
-        'data-id': 'tomohiko',
-        'data-description': 'Support me on Buy me a coffee!' ,
-        'data-message': '',
-        'data-color': '#5F7FFF',
-        'data-position': 'Right',
-        'data-x_margin': '18',
-        'data-y_margin': '18',
-    }
+  tag: 'script',
+  attrs: {
+    'data-name': 'BMC-Widget',
+    'data-cfasync': 'false',
+    src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
+    'data-id': 'tomohiko',
+    'data-description': 'Support me on Buy me a coffee!',
+    'data-message': '',
+    'data-color': '#5F7FFF',
+    'data-position': 'Right',
+    'data-x_margin': '18',
+    'data-y_margin': '18',
+  }
 };
