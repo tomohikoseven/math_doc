29fd6801dc31605068cfef23e4ba92b7d8ae18ef
tomohikoseven <tomohikoseven0905@gmail.com>
2026-05-21 17:09:22 +0900

    インスタグラムのソーシャルリンクの削除。

--- src/libs/social.ts
+++ src/libs/social.ts
@@ -1,5 +1,4 @@
 export const SOCIAL_LINKS = [
   { icon: "github", label: "GitHub", href: "https://github.com/tomohikoseven" },
   { icon: "x.com", label: "X", href: "https://x.com/tomohikoseven" },
-  { icon: "instagram", label: "Instagram", href: "https://www.instagram.com/nagasetomohiko/" },
 ];
